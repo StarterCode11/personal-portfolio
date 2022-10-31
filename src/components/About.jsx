cada7d474bc9cede178356573afa51de32b384f4
StarterCode11 <infokumarpratyush03@gmail.com>
2022-10-31 18:26:09 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi. I'm Pratyush Kumar, nice to meet you. Please take a look around.</p>
+            </div>
+            <div>
+              <p>Frontend Developer with a proven ability to collaborate effectively with
+                senior developers while spending extra time for learning. Ready to apply my passion for coding to
+                a talented engineering team to develop
+                    quality solutions.</p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+import Spotify from '../assets/projects/spotify.jpg'
+import Nft from '../assets/projects/Nft-marketplace.jpg'
+import Task from '../assets/projects/Task-app.png'
+import Movie from '../assets/projects/movie-app.jpg'
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Spotify,
+        github:"https://github.com/StarterCode11/Spotifyclone",
+        live:"https://startercode11.github.io/Spotifyclone/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Nft,
+        github:"https://github.com/StarterCode11/nft-marketplace",
+        live:"https://nft-marketplace-2i4r8onhz-startercode11.vercel.app/",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Task,
+        github:"https://github.com/StarterCode11/task-app",
+        live:"https://fanciful-monstera-dbaa26.netlify.app/",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:Movie,
+        github:"https://github.com/StarterCode11/movie-app",
+        live:"https://superlative-lebkuchen-0692b2.netlify.app/",
+    },
+
+
+]
